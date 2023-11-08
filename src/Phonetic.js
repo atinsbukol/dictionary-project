8054f09f4e94aa2e38baa1c8fb8e47beadefcd52
Bukola Agunbiade <atinsbukol@gmail.com>
2023-11-08 17:10:10 +0000

    Added "rel"

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ import "./Phonetic.css";
 export default function Phonetic (props) {
     return (
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank">Listen</a>
+            <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">Listen</a>
             
            <span className="text"> {props.phonetic.text}</span>
         </div>
